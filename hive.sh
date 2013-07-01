505abcfeda71973bdfe5a1918dc61acc7844035c
rvosa <rutgeraldo@gmail.com>
2013-07-01 11:40:41 +0200

    we now make a hive plot for each gene

--- hive.sh
+++ hive.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-DATA=data/candidates
-WORK=doc/hiveplot
+DATA=`pwd`/data/candidates
+WORK=`pwd`/doc/hiveplot
 perl script/make_hive_plot.pl -v -d $DATA -w $WORK \
 	-i U0015716 -i WAG0463703 -i U0015717 \
 	-a U0015717=black -a U0015716=blue -a WAG0463703=yellow
-linnet -conf $WORK/linnet.conf
+CONFS=`ls $WORK/*.conf`
+for CONF in $CONFS; do
+	linnet -conf $CONF
+done
