953601430dfddf99626ac3da860e410170a74c67
rvosa <rutgeraldo@gmail.com>
2014-10-13 17:30:42 +0200

    adding wrapper script to reformat previous batch of WUR data

--- wur_candidates.sh
+++ wur_candidates.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 DATA=data
-INDIR=$DATA/candidates_wur
+INDIR=$DATA/genes_by_accession
 OUTDIR=$DATA/wur_reformatted
 DB=$DATA/reference/ITAG2.3_release/ITAG2_3_genomic.fasta
 GFF=$DATA/reference/ITAG2.3_release/ITAG2.3_gene_models.gff3
-KEEP="046,051,054,073"
+KEEP="030,031,046,051,054,073"
 REFORMAT="perl script/reformat_wur_candidates.pl"
-VERBOSITY=
-GFF3=
+VERBOSITY=-v
 
 # the WUR data is such that for each accession there is a file
 # with the consensus for all candidate genes. this needs to be
 # the other way around: a file for each candidate gene, with the
 # accessions of interest
-# $REFORMAT -indir $INDIR -outdir $OUTDIR -keep $KEEP -db $DB -gff $GFF $VERBOSITY
+$REFORMAT -indir $INDIR -outdir $OUTDIR -keep $KEEP -db $DB -gff $GFF $VERBOSITY
